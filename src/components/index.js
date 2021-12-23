5327a1b2db71c6028d0a637449d2ef3c242e01c8
Swastik Singh <singhswastik29@gmail.com>
2021-12-23 10:07:24 +0530

    Exported components from index file and setup in App.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import ProductCard from "./ProductCard";
+import ProductsList from "./ProductList";
+import ProductFilters from "./ProductFilters";
+import MultiRangeSlider from "./MultiRangeSlider"
+
+export { ProductCard, ProductsList, ProductFilters, MultiRangeSlider };
